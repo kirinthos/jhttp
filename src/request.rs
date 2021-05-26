ee69b3db6a3b3f8f40dcac848ec4489009a41584
Jay Cunningham <j@crosscompiled.com>
2021-05-25 23:48:36 -0400

    working on request parsing

--- src/request.rs
+++ src/request.rs
@@ -3,8 +3,9 @@
 //!
 
 use std::collections::HashMap;
+use std::error::Error;
 
-pub use util;
+use super::util;
 
 /// HttpRequest struct that contains the raw request
 /// headers and body of an incoming request
@@ -14,14 +15,35 @@ pub struct HttpRequest {
 }
 
 impl HttpRequest {
-    fn from_str(request_body: &str) -> HttpRequest {
-        let segments = util::partition_by(|v| v == "", request_body.split("\n"));
-        let headers: HashMap<String, String> = ;
+    fn from_str(request_body: &str) -> Result<HttpRequest, Box<dyn Error>> {
+        let lines: Vec<&str> = request_body.split('\n').collect();
+        let (method, rest) = match lines.is_empty() {
+            true => Err("Request does not contain any content"),
+            false => Ok((&lines[0], &lines[1..])),
+        }?;
+
+        let body_location = rest.iter().position(|&v| v.is_empty());
+
+        let (header_lines, body_lines) = match body_location {
+            Some(v) => (&rest[..v], Some(&rest[(v + 1)..])),
+            None => (rest, None),
+        };
+
+        println!("{:?}\n{:?}\n{:?}", method, header_lines, body_lines);
+        let headers: HashMap<String, String> = header_lines
+            .iter()
+            .map(|v| {
+                v.split_once(":")
+                    .map(|(k, v)| (k.to_owned(), v.trim().to_owned()))
+            })
+            .filter(|v| v.is_some())
+            .map(|v| v.unwrap())
+            .collect();
+
         let body = String::from("body");
-        HttpRequest { headers, body }
+        Ok(HttpRequest { headers, body })
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -29,6 +51,8 @@ mod tests {
 
     #[test]
     fn test_http_request_new() {
-        let http = HttpRequest::from_str("Hello: There");
+        let http =
+            HttpRequest::from_str("POST /somewhere HTTP/1.1\nHello: There\n\ntempting: isn't it");
+        assert!(false);
     }
 }
